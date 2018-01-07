3c8fa2b13d4c3b5fe63ec28f1c6c0dd3ef8a9329
Elliott B. Edwards <el2iot2@users.noreply.github.com>
2018-01-07 12:04:57 -0800

    adds

---
+++ src/Mona/Input.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Mona
+{
+    /// <summary>
+    /// Input represents a contiguous region of memory that must be parsed in its entirety in order to be understood. 
+    /// </summary>
+    public readonly ref struct Input
+    {
+        internal Input(Uri uri, ReadOnlySpan<char> buffer)
+        {
+            Uri = uri;
+            Buffer = buffer;
+        }
+
+        public readonly ReadOnlySpan<char> Buffer;
+        public readonly Uri Uri;
+
+        public override string ToString() => $"{Uri}:{Buffer}";
+    }
+}
---
+++ src/Mona/Provide.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Mona
+{
+    public static class Provide
+    {
+        public static Input Default()
+        {
+            return new Input();
+        }
+
+        public static bool Single(Uri uri, out Input input)
+        {
+            switch (uri)
+            {
+                case Uri _ when uri.IsFile && File.Exists(uri.LocalPath):
+                    return AllText(uri.LocalPath, out input);
+                default:
+                    input = new Input();
+                    return false;
+            }
+            bool AllText(string path, out Input result)
+            {
+               var buffer = File.ReadAllText(path);
+               result = new Input(uri, buffer.AsSpan());
+               return true;
+            }
+        }
+    }
+}
